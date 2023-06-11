2b470be9c262528757de840c59476bdaad26aaaf
Broodje-unox <104534482+Broodje-unox@users.noreply.github.com>
2023-06-11 16:10:58 +0900

    Made home.dart
    Added a Folder for the home screen and started working on styling the page.
    
    Reverted startting point of the app to the home screen.

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'lib/screens/loginScreen/login_page.dart';
+import 'lib/Screens/HomeScreen/home.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
